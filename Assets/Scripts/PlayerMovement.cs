0eabcdc44f48063e6c57fd9bf3aad7a0671a850d
ToTheCosmos <105660082+HarveyLGreener@users.noreply.github.com>
2023-11-29 11:51:17 -0500

    Started Gray Boxing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,21 +5,10 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 	public CharacterController CC;
-	public float MouseSensitivity;
-	public Transform CamTransform;
 	public float MoveSpeed = 10.0f;
 	public float SpeedMultiplier = 1.0f;
 	public float Gravity = -9.8f;
 	public float JumpSpeed;
-
-	public float CamRotation;
-	public float VerticalSpeed;
-	
-	private void Start()
-    {
-		Cursor.lockState = CursorLockMode.Locked;
-	}
-
 	private void Update()
 	{
 		Vector3 movement = Vector3.zero;
@@ -31,14 +20,6 @@ public class PlayerMovement : MonoBehaviour
 		movement += (transform.forward * forwardMovement) + (transform.right * sideMovement);
 
 		CC.Move(movement);
-
-		float mouseInputY = Input.GetAxis("Mouse Y") * MouseSensitivity;
-		CamRotation -= mouseInputY;
-		CamRotation = Mathf.Clamp(CamRotation, -90f, 90f);
-		CamTransform.localRotation = Quaternion.Euler(new Vector3(CamRotation, 0f, 0f));
-
-		float mouseInputX = Input.GetAxis("Mouse X") * MouseSensitivity;
-		transform.rotation = Quaternion.Euler(transform.eulerAngles + new Vector3(0f, mouseInputX, 0f));
 
 	}
 }
